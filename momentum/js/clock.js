e095cbc44be6c0bb64f9d98cb0f73e9ba4f8312d
kunwood <gwkim9205@naver.com>
2023-08-23 21:38:14 +0900

    배경사진 변경 & CSS 수정

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -15,7 +15,7 @@ function getClock(){
     const hours = String(time.getHours()).padStart(2, '0');
     const minutes = String(time.getMinutes()).padStart(2, '0');
     const seconds = String(time.getSeconds()).padStart(2, '0');
-    clock.innerText=`${hours}:${minutes}:${seconds}`;
+    clock.innerText=`${hours}:${minutes}`;
 }
 getDate();
 getClock();
